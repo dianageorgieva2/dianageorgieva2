253bac03e68b04599a3b03f54349da038cbf9fef
dianageorgieva2 <32527999+dianageorgieva2@users.noreply.github.com>
2023-03-07 19:47:57 +0200

    Add files via upload

--- Snake game/scoreboard.py
+++ Snake game/scoreboard.py
@@ -2,31 +2,35 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Ariel", 8, "normal")
 
+
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.pencolor("white")
         self.penup()
         self.hideturtle()
         self.goto(x=0, y=270)
         self.update_scoreboard()
-        
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score}, Highest Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
-        # self.restart()
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.high_score))
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
     def restart(self):
         self.goto(0, -20)
         self.write("Do you want to play again - y or n?", align=ALIGNMENT, font=FONT)
-
